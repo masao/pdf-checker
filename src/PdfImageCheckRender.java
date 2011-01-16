148c8888b964f717fe7abd60f39daa36f9635087
Masao Takaku <tmasao@acm.org>
2011-01-16 19:37:23 +0900

    image filetype output added.

--- src/PdfImageCheckRender.java
+++ src/PdfImageCheckRender.java
@@ -1,7 +1,8 @@
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-
+import java.util.Set;
+import java.util.Iterator;
 import java.awt.image.BufferedImage; 
 import javax.imageio.ImageIO;
  
@@ -49,8 +50,19 @@ public class PdfImageCheckRender implements RenderListener {
     public void renderImage(ImageRenderInfo renderInfo) {
         try {
             PdfImageObject img_obj = renderInfo.getImage();
-            PdfName filter = (PdfName)img_obj.get(PdfName.FILTER);
+            //PdfName filter = (PdfName)img_obj.get(PdfName.FILTER);
+	    //System.out.println( "Image filter:\t" + filter );
+	    //System.out.println( "Image dictionary:\t" + img_obj.getDictionary() );
+// 	    Set keys = img_obj.getDictionary().getKeys();
+// 	    Iterator i = keys.iterator();
+// 	    while( i.hasNext() ) {
+// 		System.out.println( i.next().toString() );
+// 	    }
+	    //int width = img_obj.getDictionary().getAsNumber( PdfName.WIDTH ).intValue();
+	    //int height = img_obj.getDictionary().getAsNumber( PdfName.HEIGHT ).intValue();
+	    //System.out.println( width + "x" + height );
 	    Image image = Image.getInstance( img_obj.getImageAsBytes() );
+	    System.out.println( "Image filetype:\t" + img_obj.getFileType() );
 	    if ( image.getDpiX() > 0 && image.getDpiX() > 0 ) {
 		System.out.println( "DPI-X:\t" + image.getDpiX() );
 		System.out.println( "DPI-Y:\t" + image.getDpiY() );
